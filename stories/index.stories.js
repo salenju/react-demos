fbcda13ac56c0465c278106dc8a1e8a98ea98d57
salenju <judc@bwoil.com>
2020-03-08 11:06:07 +0800

    update

---
+++ stories/index.stories.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Example from '../src'
+
+const {
+  RowCrollDemo, RowCrollDemo01, RowCrollDemo02, RowCrollDemo03,
+  CheckBoxDemo01,
+  CallbackRef01, CallbackRef02,
+  CustomTextInput,
+  AutoFocusTextInput,
+  Context01,
+  Link01,
+  Computed,
+  Cascader,
+} = Example
+
+storiesOf('RowCroll', module)
+  .add('-RowCroll-Hook', () => <RowCrollDemo height={200} speed={25} />)
+  .add('-RowCroll-Hook操作DOM', () => <RowCrollDemo01 height={200} speed={25} />)
+  .add('-RowCroll-class', () => <RowCrollDemo02 height={200} speed={25} />)
+  .add('-RowCroll-间歇滚动', () => <RowCrollDemo03 />)
+
+storiesOf('CheckBox', module)
+  .add('CheckBox', () => <CheckBoxDemo01 />)
+
+storiesOf('Refs', module)
+  .add('CallbackRef01', () => <CallbackRef01 />)
+  .add('CallbackRef02', () => <CallbackRef02 />)
+  .add('CustomTextInput', () => <CustomTextInput />)
+  .add('AutoFocusTextInput', () => <AutoFocusTextInput />)
+
+storiesOf('Context', module)
+  .add('Context01', () => <Context01 />)
+
+storiesOf('ReactRouter', module)
+  .add('Link01', () => <Link01 />)
+
+storiesOf('Mobx Demo', module)
+  .add('Computed', () => <Computed />)
+
+storiesOf('Cascader', module)
+  .add('Cascader', () => <Cascader />)
