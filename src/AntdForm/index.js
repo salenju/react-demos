872c53852e80bd2ef58bad5199f66c3c1e990f73
salenju <judc@bwoil.com>
2020-05-29 22:35:14 +0800

    update: update antd form

--- src/AntdForm/index.js
+++ src/AntdForm/index.js
@@ -15,6 +15,30 @@ const AntdForm = Form.create()((props) => {
     })
   }
 
+  const validateToNextPassword = (rule, value, callback) => {
+    const form = props.form
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d$@$!%*#?&_]{8,16}$/
+    if (!regex.test(value)) {
+      let msg = i18n.Text('myaccount.common.use8to16CharactersForYourPassword')
+      callback(msg)
+    }
+    if (value && store.confirmDirty) {
+      form.validateFields(['confirm'], { force: true })
+    }
+    callback()
+  }
+
+  // 校验前后两次密码是否一致
+  const compareToFirstPassword = (rule, value, callback) => {
+    const form = props.form
+    if (value && value !== form.getFieldValue('password')) {
+      let msg = i18n.Text('myaccount.common.twoPasswordsInconsistent')
+      callback(msg)
+    } else {
+      callback()
+    }
+  }
+
   return (
     <Form onSubmit={handleSubmit}>
       <FormItem label="Test">
@@ -42,7 +66,7 @@ const AntdForm = Form.create()((props) => {
               },
               {
                 validator: (rule, value, callback) =>
-                  validateToNextPassword(rule, value, callback),
+                compareToFirstPassword(rule, value, callback),
               },
             ],
           })(<InputNumber />)}
