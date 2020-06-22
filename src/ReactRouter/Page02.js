9bed7dbf821cb98ecb0071a29e1750648ac5cec4
salenju <judc@bwoil.com>
2020-06-22 14:07:32 +0800

    update: add react-router components

---
+++ src/ReactRouter/Page02.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page02  = () => {
+
+    return (
+        <div>
+            Page02
+        </div>
+    )
+}
+
+export default Page02
---
+++ src/ReactRouter/Page03.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page03  = () => {
+
+    return (
+        <div>
+            Page03
+        </div>
+    )
+}
+
+export default Page03
