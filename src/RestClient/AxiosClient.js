7fae439c00ab85f8884fe24fc63a5c75d5826f54
salenju <judc@bwoil.com>
2020-08-19 20:18:57 +0800

    update: 添加API调用封装

---
+++ src/RestClient/AxiosClient.js
@@ -0,0 +1,13 @@
+/**
+ * Axios
+ * 
+ * Q1: Axios 和 Ajax有什么区别？
+ * A1: Axios是对Ajax的进一步封装，增加了Promise，让调用更为方便。本质其实还是 Ajax。
+ * 
+ * Q2: Axios 和 Fetch有什么区别？
+ * A2: Axios只是对 Ajax的进一步封装，而 Fetch则是新的内置请求方法。
+ * 
+ * 
+ */
+
+
