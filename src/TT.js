8f1ed800a03badb1e73838fb9085e90bd5858228
salenju <judc@bwoil.com>
2020-10-29 08:43:18 +0800

    update

--- src/TT.js
+++ src/TT.js
@@ -24,3 +24,28 @@ const PromiseCopy = function (fn) {
   }
   fn(resolve, reject)
 }
+
+// 元素批量修改
+
+// bad
+box.style.width = '200px'
+box.style.height = '200px'
+
+// good
+box.style.cssText = 'width: 200px; height: 200px'
+// 或
+box.className = 'aa'
+
+// 读写分离
+
+// bad
+box.style.width = '200px'
+box.style.height = '200px'
+console.log(box.clientWidth)
+box.style.margin = '10px'
+
+// good
+box.style.width = '200px'
+box.style.height = '200px'
+box.style.margin = '10px'
+console.log(box.clientWidth)
